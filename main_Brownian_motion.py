1f27580aadab402df366fe244c4c68d79e8fba70
Silvia Vargas <silvia.vargas@studio.unibo.it>
2020-08-06 07:53:08 +0200

    commented configuration version and initialized parameters in main_Brownian_motion.py

--- main_Brownian_motion.py
+++ main_Brownian_motion.py
@@ -9,6 +9,7 @@ import numpy as np
 import sys
 import configparser
 
+"""
 #main part of the code
 config = configparser.ConfigParser()
 config.read(sys.argv[1])
@@ -26,12 +27,20 @@ N = int(N)
 T = int(T)
 h = float (h)
 dt = float(dt)
+"""
+
+N = 100 # the number of discrete steps 
+T = 1 # the number of continuous time steps
+h = 1 # the variance of the increments
+dt = 1.0 * T/N # total number of time steps
 
 
 
 # generate a brownian motion
 X, dX = brownian_motion(N, T ,h)
 t = np.linspace(0.0, N*dt, N+1)
-
+"""
 np.save(destination1,t)
 np.save(destination2,X)
+"""
+
