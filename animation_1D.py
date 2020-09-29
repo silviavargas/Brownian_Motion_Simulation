49ba9c0c9bc6f5b16108addce0824ef99cb87c6a
Silvia Vargas <silvia.vargas@studio.unibo.it>
2020-09-29 12:26:36 +0200

    resized the gif brownian_motion_animation_1D.gif

--- animation_1D.py
+++ animation_1D.py
@@ -9,7 +9,7 @@ import matplotlib.animation as animation
 import numpy as np
 from Brownian_motion import brownian_motion
 
-fig = plt.figure(figsize=(21, 10))
+fig = plt.figure(figsize=(15, 7))
 ax = plt.axes(xlim=(0, 1)) 
 line, = ax.step([], [], where='mid', color='#0492C2')
     
--- main_Brownian_motion.py
+++ main_Brownian_motion.py
@@ -6,28 +6,9 @@ Created on Wed Aug  5 18:13:19 2020
 """
 from Brownian_motion import brownian_motion
 import numpy as np
-#import sys
-#import configparser
 
-"""
+
 #main part of the code
-config = configparser.ConfigParser()
-config.read(sys.argv[1])
-
-N = config.get('parameters', 'N')
-T= config.get('parameters', 'T')
-h = config.get('parameters', 'h')
-dt = config.get('parameters', 'dt')
-
-destination1 = config.get('paths','my_time')
-destination2 = config.get('paths','my_motion')
-
-
-N = int(N)
-T = int(T)
-h = float (h)
-dt = float(dt)
-"""
 
 N = 100 # the number of discrete steps 
 T = 1 # the number of continuous time steps
@@ -42,8 +23,5 @@ X,dX = brownian_motion(N, T ,h)
 t = np.linspace(0.0, N*dt, N+1)
 
 
-"""
-np.save(destination1,t)
-np.save(destination2,X)
-"""
+
 
